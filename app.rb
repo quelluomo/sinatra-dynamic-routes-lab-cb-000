4fd342ff42e600920b8bba206b2f6ad9c13782b4
Matteo Muscas <thatman.j@gmail.com>
2019-03-15 09:40:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
-    @reverse = params[:name].reverse 
+    @reverse = params[:name].reverse
     "#{@reverse}"
   end
 end
