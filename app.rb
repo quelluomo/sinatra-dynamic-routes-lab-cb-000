b85de8e45040ef97b70046368d8f855e3fbc4f56
Matteo Muscas <thatman.j@gmail.com>
2019-03-15 09:42:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,9 @@ class App < Sinatra::Base
     @reverse = params[:name].reverse
     "#{@reverse}"
   end
+
+  get "/square/:num" do
+    @square = params[:num] * params[:num]
+    "#{@square}"
+  end
 end
