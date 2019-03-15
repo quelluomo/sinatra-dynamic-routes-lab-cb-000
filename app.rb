996272f5c20253829d1841a96c9bab5fe666fdf9
Matteo Muscas <thatman.j@gmail.com>
2019-03-15 10:38:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get "/operation/:number1/:number2" do
+  get "/:operation/:number1/:number2" do
     @add = params[:number1].to_i + params[:number2].to_i
     @sub = params[:number1].to_i - params[:number2].to_i
     @mult = params[:number1].to_i * params[:number2].to_i
