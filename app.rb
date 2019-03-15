72ec643a1ee10cbb0ac174b0201e8f4946ee2bbf
Matteo Muscas <thatman.j@gmail.com>
2019-03-15 10:33:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,13 +26,13 @@ class App < Sinatra::Base
 
   get "/operation/:number1/:number2" do
     @add = params[:number1].to_i + params[:number2].to_i
-    @sub = params[:number1].to_i - params[:number2].to_i
-    @mult = params[:number1].to_i * params[:number2].to_i
-    @div = params[:number1].to_i / params[:number2].to_i
+    #@sub = params[:number1].to_i - params[:number2].to_i
+    #@mult = params[:number1].to_i * params[:number2].to_i
+    #@div = params[:number1].to_i / params[:number2].to_i
       "#{@add}"
-      "#{@sub}"
-      "#{@mult}"
-      "#{@div}"
+      #{}"#{@sub}"
+    #  "#{@mult}"
+    #  "#{@div}"
     end
 
 end
