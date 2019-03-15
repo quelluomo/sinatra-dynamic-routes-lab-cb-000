a855c79572422cb894c2b2ab78db897d25110f40
Matteo Muscas <thatman.j@gmail.com>
2019-03-15 10:12:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @num = params[:number].to_i
     @phr = params[:phrase]
-    @sentence = @num.times do  
+    @sentence = @num.times do
       puts "#{@phr}"
     end
     "#{@sentence}"
